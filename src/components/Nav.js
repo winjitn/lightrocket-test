dd4cef9b658440300e58a716ba4513e3e15ae1d1
win <winjitn@gmail.com>
2020-04-15 19:12:42 +0700

    fixed navbar bug

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -19,6 +19,7 @@ const Nav = (props) => {
               </li>
               <li>
                 <NavLink
+                  exact
                   activeClassName="link-active"
                   className="nav-item"
                   to={`/${user}`}
